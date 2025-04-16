21311eb07cad3a5d517f828f4d98dfc06782a857
gy601057 <gy601057@gmail.com>
2025-04-16 18:18:16 +0000

    feat: speech to text

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 void main() {
   runApp(const MyApp());
@@ -45,14 +47,119 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                flutterTts.speak("Hello");
-              },
-              child: Text('Say Hello'),
-            ),
+            SayHelloWidget(),
+            SizedBox(height: 10),
+            SpeechToTextWidget(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class SayHelloWidget extends StatefulWidget {
+  const SayHelloWidget({super.key});
+
+  @override
+  State<SayHelloWidget> createState() => _SayHelloWidgetState();
+}
+
+class _SayHelloWidgetState extends State<SayHelloWidget> {
+  FlutterTts flutterTts = FlutterTts();
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        flutterTts.speak("Hello");
+      },
+      child: Text('Say Hello'),
+    );
+  }
+}
+
+class SpeechToTextWidget extends StatefulWidget {
+  @override
+  State<SpeechToTextWidget> createState() => _SpeechToTextWidgetState();
+}
+
+class _SpeechToTextWidgetState extends State<SpeechToTextWidget> {
+  final stt.SpeechToText _speechToText = stt.SpeechToText();
+
+  bool _speechEnabled = false;
+
+  String _lastWords = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _initSpeech();
+  }
+
+  /// This has to happen only once per app
+  void _initSpeech() async {
+    _speechEnabled = await _speechToText.initialize();
+    setState(() {});
+  }
+
+  /// Each time to start a speech recognition session
+  void _startListening() async {
+    await _speechToText.listen(onResult: _onSpeechResult);
+    setState(() {});
+  }
+
+  /// Manually stop the active speech recognition session
+  /// Note that there are also timeouts that each platform enforces
+  /// and the SpeechToText plugin supports setting timeouts on the
+  /// listen method.
+  void _stopListening() async {
+    await _speechToText.stop();
+    setState(() {});
+  }
+
+  /// This is the callback that the SpeechToText plugin calls when
+  /// the platform returns recognized words.
+  void _onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      _lastWords = result.recognizedWords;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Text('Recognized words:', style: TextStyle(fontSize: 20.0)),
+          ),
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Text(
+              // If listening is active show the recognized words
+              _speechToText.isListening
+                  ? _lastWords
+                  // If listening isn't active but could be tell the user
+                  // how to start it, otherwise indicate that speech
+                  // recognition is not yet ready or not supported on
+                  // the target device
+                  : _speechEnabled
+                  ? 'Tap the microphone to start listening...'
+                  : 'Speech not available',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // If not yet listening for speech start, otherwise stop
+              _speechToText.isNotListening ? _startListening : _stopListening;
+            },
+            child: Icon(
+              _speechToText.isNotListening ? Icons.mic_off : Icons.mic,
+            ),
+          ),
+        ],
       ),
     );
   }
